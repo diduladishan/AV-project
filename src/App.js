61b6238ce2f4e76822a0f4a8f17e4769ae14d6ad
diduladishan <diduladishan14@gmail.com>
2023-10-13 08:20:22 +0530

    logo added

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function NavigationBar() {
         />
       </div>
       <div>
-        <p>Events Pagee </p>
+        <p>Events Page </p>
       
       </div>
 
@@ -66,7 +66,7 @@ function HeroSectionOne() {
           The worlds first AV Event Bidding Platform 
         </p>
         <p className="font-poppins font-extrabold text-6xl mb-6 text-[#181059] leading-snug">
-          Connecting Event Managers with AV Providers
+          Connecting Event Managers with <span className="text-[#FF5533]">AV Providers</span>
         </p>
         <button className="bg-[#181059] text-white rounded-md py-2 px-4 text-center font-bold text-lg hover:bg-[#181059] focus:outline-none focus:ring-2 focus:ring-[#181059]">Get Started</button>
       </div>
@@ -228,19 +228,14 @@ function WantToGetMore() {
 
       <div className="flex justify-center items-center bg-[#957FEF] rounded-md mt-16">
         <div>
-        <div className="font-poppins font-bold text-[45px] mb-4">Want to Get More Clients?</div>
-        <div className="font-poppins font-normal text-[22px] mb-4">Browse the events listing page, and submit your proposal. No more cold calling or emailing for new clients.</div>
+        <div className="font-poppins font-bold text-[45px] mb-4">Want to Get More <span className="text-[#fff]">Clients?</span></div>
+        <div className="font-poppins font-normal text-[22px] text-[#fff] mb-4">Browse the events listing page, and submit your proposal. No more cold calling or emailing for new clients.</div>
         <div>
         <button className="bg-[#181059] text-white rounded-md py-2 px-4 text-center font-bold text-lg hover:bg-[#181059] focus:outline-none focus:ring-2 focus:ring-[#181059]">Learn More</button>
     </div>
 
         </div>
-      
-
-
-
-
-
+    
       <div> 
         <img
           src="https://diduladishan.github.io/AV-project/home_page/want-to-get-more.png"
@@ -265,7 +260,7 @@ function Footer() {
 <div className="flex justify-between ">
       <div>
       <img
-          src="https://diduladishan.github.io/AV-project/home_page/logo.png"
+          src="https://diduladishan.github.io/AV-project/home_page/aa.png"
           alt="logo"
           style={{
             width: "200px", // Set the desired width
